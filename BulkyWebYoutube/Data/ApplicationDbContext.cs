be8c55710dc32b605b61bb1416459df1c2ef2325
chetannikam <nikamchetan1998@gmail.com>
2023-09-08 23:54:11 +0530

    2.Created AppicationDBCOntext class which takes options from AddDbConextService which passes connectionstring to it

---
+++ BulkyWebYoutube/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebYoutube.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options )
+        {
+            
+        }
+    }
+}
