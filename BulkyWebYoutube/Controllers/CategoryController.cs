7e2826b9f3bab0be20e82926930984dd671871a5
chetannikam <nikamchetan1998@gmail.com>
2023-09-10 19:26:30 +0530

    Added Create Category with client and server validation

--- BulkyWebYoutube/Controllers/CategoryController.cs
+++ BulkyWebYoutube/Controllers/CategoryController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWebYoutube.Data;
+using BulkyWebYoutube.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWebYoutube.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories=_db.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create() 
+        {
+        return View(new Category());
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order Can not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+                
+            }
+            
         }
     }
 }
--- BulkyWebYoutube/Models/Category.cs
+++ BulkyWebYoutube/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWebYoutube.Models
 {
@@ -7,8 +8,11 @@ namespace BulkyWebYoutube.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(50)]
         public string Name { get; set; }
-        
+        [Range(1,100)]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
