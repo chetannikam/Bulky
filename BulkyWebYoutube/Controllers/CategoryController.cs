6201c29c084e3e52af1b9882e6eede8f5607ae63
chetannikam <nikamchetan1998@gmail.com>
2023-09-16 12:48:53 +0530

    delete operation completed

--- BulkyWebYoutube/Controllers/CategoryController.cs
+++ BulkyWebYoutube/Controllers/CategoryController.cs
@@ -40,5 +40,69 @@ namespace BulkyWebYoutube.Controllers
             }
             
         }
+        public IActionResult Edit(int ?Id)
+        {
+            if (Id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order Can not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+
+        public IActionResult DeletePost(int? Id)
+        {
+
+            Category categoryFromDB = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if(categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Categories.Remove(categoryFromDB);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Category");
+                }
+                else
+                {
+                    return View();
+
+                }
+            }
+            
+        }
     }
 }
--- BulkyWebYoutube/Models/Category.cs
+++ BulkyWebYoutube/Models/Category.cs
@@ -10,7 +10,7 @@ namespace BulkyWebYoutube.Models
         [Required]
         [DisplayName("Category Name")]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string ?Name { get; set; }
         [Range(1,100)]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
